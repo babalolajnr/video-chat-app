9cf714ca7be92ca4608275ead039d4f5c82446de
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-04 11:50:53 +0100

    minor change

--- public/script.js
+++ public/script.js
@@ -4,8 +4,10 @@ const myPeer = new Peer(undefined, {
     host: '/',
     port: '3001'
 })
+
 const myVideo = document.createElement('video')
 myVideo.muted = true
+
 const peers = {}
 
 navigator.mediaDevices.getUserMedia({
